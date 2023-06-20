6eaf4e632f527756e1b2b15b28bed0bf315c81f1
Partha <parthasarker3@gmail.com>
2023-06-21 00:10:31 +0600

    Add eslint, prettier support

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,18 +1,16 @@
-import './globals.css'
+import React from "react";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+      <head>
+        <title></title>
+      </head>
+      <body className="p-2">{children}</body>
     </html>
-  )
+  );
 }
